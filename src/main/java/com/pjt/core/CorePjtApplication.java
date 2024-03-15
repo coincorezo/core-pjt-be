8dd8ac5138a7671f839cc2bf108b6a0a4f13b5eb
zeroh0 <umberto1900@naver.com>
2024-03-15 23:09:22 +0900

    init

---
+++ src/main/java/com/pjt/core/CorePjtApplication.java
@@ -0,0 +1,13 @@
+package com.pjt.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorePjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorePjtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pjt/core/CorePjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjt.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorePjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
