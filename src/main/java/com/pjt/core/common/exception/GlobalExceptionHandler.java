ba1b31e357731751c596a048a2291fead1461069
zeroh0 <umberto1900@naver.com>
2024-03-17 06:55:23 +0900

    Feat: 입력값 유효성 예외 처리

--- src/main/java/com/pjt/core/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pjt/core/common/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.pjt.core.common.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,6 +11,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+		log.error("::handleMethodArgumentNotValidException::", e);
+		ExceptionResponse exceptionResponse = ExceptionResponse.of(ErrorCode.INVALID_INPUT_VALUE, e.getBindingResult());
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
 
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ExceptionResponse> handleException(Exception e) {
