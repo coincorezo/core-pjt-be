0ddfb3f836805eb5ebcbe12c862cafa0f9f24906
zeroh0 <umberto1900@naver.com>
2024-03-23 00:42:41 +0900

    Feat: 존재하지 않는 데이터 에러메시지 추가

--- src/main/java/com/pjt/core/common/error/exception/NoDataException.java
+++ src/main/java/com/pjt/core/common/error/exception/NoDataException.java
@@ -1,4 +1,11 @@
 package com.pjt.core.common.error.exception;
 
+import com.pjt.core.common.error.response.ErrorCode;
+
 public class NoDataException extends RuntimeException {
+
+	public NoDataException(ErrorCode errorCode) {
+		super(errorCode.getMessage());
+	}
+
 }
--- src/main/java/com/pjt/core/common/error/response/ErrorCode.java
+++ src/main/java/com/pjt/core/common/error/response/ErrorCode.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
+	NO_DATA("common", "데이터가 존재하지 않습니다."),
 	INVALID_INPUT_VALUE("common", "유효하지 않는 입력 값입니다."),
 	INTERNAL_SERVER_ERROR("common", "서버 에러가 발생하였습니다.")
 	;
