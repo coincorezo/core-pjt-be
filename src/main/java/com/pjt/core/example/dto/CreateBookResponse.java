2302f89721b28c7bc22a309807d0fc8289eeedf0
zeroh0 <umberto1900@naver.com>
2024-04-17 17:45:23 +0900

    Docs: BookController swagger 상세설정
    - BookController @Tag 추가
    - 각 api에 @Operation 추가
    - @PathVariable을 사용하는 경우 @Parameter 추가
    - request & response dto 필드에 @Schema 추가

--- src/main/java/com/pjt/core/example/dto/CreateBookResponse.java
+++ src/main/java/com/pjt/core/example/dto/CreateBookResponse.java
@@ -2,6 +2,7 @@ package com.pjt.core.example.dto;
 
 import com.pjt.core.example.entity.Book;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,16 +14,22 @@ import java.time.LocalDateTime;
 @Builder
 public class CreateBookResponse {
 
+	@Schema(description = "책 id")
 	private Long id;
 
+	@Schema(description = "책 코드")
 	private String bookCode;
 
+	@Schema(description = "책 코드")
 	private String title;
 
+	@Schema(description = "책 부제")
 	private String subtitle;
 
+	@Schema(description = "글쓴이")
 	private String writer;
 
+	@Schema(description = "등록시간")
 	private LocalDateTime registeredDateTime;
 
 	public static CreateBookResponse fromEntity(Book book) {
