922afcd8bca50e47a181bdd25e3f09f19f3e7b78
DESKTOP-6L95I5B\밍지 <gmralwnr22@gmail.com>
2024-05-03 11:13:00 +0900

    댓글 등록 req notBlack 추가

--- src/main/java/com/pjt/core/board/dto/CreateReplyReqDto.java
+++ src/main/java/com/pjt/core/board/dto/CreateReplyReqDto.java
@@ -1,6 +1,7 @@
 package com.pjt.core.board.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 import java.security.Timestamp;
@@ -11,18 +12,21 @@ public class CreateReplyReqDto {
     private int replyId;
 
     @Schema(description="게시판 ID")
+    @NotBlank(message = "게시판 ID 필수입니다.")
     private int boardId;
 
     @Schema(description="댓글 내용")
+    @NotBlank(message = "댓글 내용 필수입니다.")
     private String replyComment;
 
     @Schema(description="등록일자")
-    private Timestamp regDt;
+    private String regDt;
 
     @Schema(description="수정일자")
-    private Timestamp updateDt;
+    private String updateDt;
 
-    @Schema(description="삭제 여부")
+    @Schema(description="삭제 여부 Y/N")
+    @NotBlank(message = "삭제 여부 필수입니다.")
     private String useYn;
 
 }
