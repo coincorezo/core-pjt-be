8c6ac4c47edb7b5a918cd37757abc004333ef30e
DESKTOP-6L95I5B\밍지 <gmralwnr22@gmail.com>
2024-05-22 18:07:54 +0900

    코인 insert 추가

--- src/main/java/com/pjt/core/coin/service/CoinService.java
+++ src/main/java/com/pjt/core/coin/service/CoinService.java
@@ -5,6 +5,8 @@ import com.pjt.core.coin.dto.CreateCoinResDto;
 import com.pjt.core.coin.dto.PointsHistoryReqDto;
 import com.pjt.core.coin.dto.PointsHistoryResDto;
 import com.pjt.core.coin.repository.CoinMapper;
+import com.pjt.core.user.dto.CurrentUser;
+import com.pjt.core.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,10 @@ import org.springframework.stereotype.Service;
 public class CoinService {
     @Autowired
     CoinMapper coinMapper;
+
+    @Autowired
+    UserService userService;
+
 
     public PointsHistoryResDto getCoinSearch(PointsHistoryReqDto pointsHistoryReqDto) {
         PointsHistoryResDto pointsHistoryResDto = new PointsHistoryResDto();
@@ -21,6 +27,12 @@ public class CoinService {
 
     public CreateCoinResDto saveCoin(CreateCoinReqDto coinReqDto) {
         CreateCoinResDto coinResDto = new CreateCoinResDto();
+        CurrentUser currentUser = userService.getLoginUser();
+        if (currentUser.getId().equals(coinReqDto.getUserId())) {
+            coinReqDto.setUserId(currentUser.getId());
+            // coinReqDto.
+
+        }
         coinMapper.saveCoin(coinReqDto);
 
 
