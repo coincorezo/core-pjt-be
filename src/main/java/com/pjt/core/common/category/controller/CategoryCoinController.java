cb80bf09be476529152ed46aef27e866a0df5c96
zeroh0 <umberto1900@naver.com>
2024-05-23 00:13:12 +0900

    Comment: 카테고리별 코인 조회 & 등록 API

--- src/main/java/com/pjt/core/common/category/controller/CategoryCoinController.java
+++ src/main/java/com/pjt/core/common/category/controller/CategoryCoinController.java
@@ -16,6 +16,10 @@ public class CategoryCoinController {
 
 	private final CategoryCoinService categoryCoinService;
 
+	/**
+	 * 카테고리별 코인 목록 조회 API
+	 * @return 카테고리별 코인 목록
+	 */
 	@GetMapping("/category-coin")
 	public ApiResponse<List<CategoryCoin>> getCategoryCoin() {
 		List<CategoryCoin> categoryCoinList = categoryCoinService.getCategoryCoin();
@@ -23,6 +27,11 @@ public class CategoryCoinController {
 		return ApiResponse.ok(categoryCoinList);
 	}
 
+	/**
+	 * 카테고리별 코인 등록 API
+	 * @param request 카테고리별 코인 생성
+	 * @return 카테고리별 코인 등록 결과
+	 */
 	@PostMapping("/category-coin")
 	public ApiResponse<Void> createCategoryCoin(@RequestBody CreateCategoryCoinRequest request) {
 		categoryCoinService.createCategoryCoin(request);
