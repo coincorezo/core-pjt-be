836b6face36bea0f4c466cd830fc7356cb9c0d24
zeroh0 <umberto1900@naver.com>
2024-05-23 09:37:30 +0900

    Feat: 로그인 사용자 검증 메소드 추가

--- src/main/java/com/pjt/core/user/dto/CurrentUser.java
+++ src/main/java/com/pjt/core/user/dto/CurrentUser.java
@@ -23,6 +23,15 @@ public class CurrentUser {
 		this.userLevel = userLevel;
 	}
 
+	/**
+	 * 현재 사용자의 ID와 매개변수로 받은 ID가 같은지 확인
+	 * @param id 사용자 ID
+	 * @return 같으면 true, 다르면 false
+	 */
+	public boolean isEqualsId(String id) {
+		return this.id.equals(id);
+	}
+
 	public static CurrentUser fromEntity(User user) {
 		return CurrentUser.builder()
 				.id(user.getUserId())
